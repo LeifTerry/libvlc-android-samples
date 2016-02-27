92d8a7f6d9d816ded89a0f4f2db20d2576cfb5fa
Thomas Guillem <thomas@gllm.fr>
2016-02-27 21:48:26 +0100

    add java_sample

---
+++ java_sample/src/main/java/org/videolan/javasample/JavaActivity.java
@@ -0,0 +1,271 @@
+/*****************************************************************************
+ * JavaActivity.java
+ *****************************************************************************
+ * Copyright (C) 2016 VideoLAN
+ * All rights reserved.
+ *
+ * This software may be modified and distributed under the terms
+ * of the BSD license. See the LICENSE file for details.
+ *****************************************************************************/
+
+package org.videolan.javasample;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.os.Build;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.SurfaceView;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.ViewStub;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import org.videolan.libvlc.IVLCVout;
+import org.videolan.libvlc.LibVLC;
+import org.videolan.libvlc.Media;
+import org.videolan.libvlc.MediaPlayer;
+
+import java.util.ArrayList;
+
+public class JavaActivity extends AppCompatActivity implements IVLCVout.Callback {
+    private static final boolean ENABLE_SUBTITLES = true;
+    private static final String TAG = "JavaActivity";
+    private static final String SAMPLE_PATH = "/sdcard/Movies/sample.mp4";
+    private static final int SURFACE_BEST_FIT = 0;
+    private static final int SURFACE_FIT_HORIZONTAL = 1;
+    private static final int SURFACE_FIT_VERTICAL = 2;
+    private static final int SURFACE_FILL = 3;
+    private static final int SURFACE_16_9 = 4;
+    private static final int SURFACE_4_3 = 5;
+    private static final int SURFACE_ORIGINAL = 6;
+    private static int CURRENT_SIZE = SURFACE_BEST_FIT;
+
+    private SurfaceView mUiSurface = null;
+    private FrameLayout mVideoSurfaceFrame = null;
+    private SurfaceView mVideoSurface = null;
+    private SurfaceView mSubtitlesSurface = null;
+
+    private final Handler mHandler = new Handler();
+    private View.OnLayoutChangeListener mOnLayoutChangeListener = null;
+
+    private LibVLC mLibVLC = null;
+    private MediaPlayer mMediaPlayer = null;
+    private int mVideoHeight = 0;
+    private int mVideoWidth = 0;
+    private int mVideoVisibleHeight = 0;
+    private int mVideoVisibleWidth = 0;
+    private int mVideoSarNum = 0;
+    private int mVideoSarDen = 0;
+
+    @TargetApi(19)
+    private void hide() {
+        // Note that some of these constants are new as of API 16 (Jelly Bean)
+        // and API 19 (KitKat). It is safe to use them, as they are inlined
+        // at compile-time and do nothing on earlier devices.
+        mUiSurface.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        final ArrayList<String> args = new ArrayList<>();
+        args.add("-vvv");
+        mLibVLC = new LibVLC(args);
+        mMediaPlayer = new MediaPlayer(mLibVLC);
+
+        mUiSurface = (SurfaceView) findViewById(R.id.ui_surface);
+        mVideoSurfaceFrame = (FrameLayout) findViewById(R.id.video_surface_frame);
+        mVideoSurface = (SurfaceView) findViewById(R.id.video_surface);
+        if (ENABLE_SUBTITLES) {
+            final ViewStub stub = (ViewStub) findViewById(R.id.subtitles_stub);
+            mSubtitlesSurface = (SurfaceView) stub.inflate();
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mMediaPlayer.release();
+        mLibVLC.release();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        final IVLCVout vlcVout = mMediaPlayer.getVLCVout();
+        vlcVout.setVideoView(mVideoSurface);
+        if (mSubtitlesSurface != null)
+            vlcVout.setSubtitlesView(mSubtitlesSurface);
+        vlcVout.attachViews();
+        mMediaPlayer.getVLCVout().addCallback(this);
+        
+        Media media = new Media(mLibVLC, SAMPLE_PATH);
+        mMediaPlayer.setMedia(media);
+        media.release();
+        mMediaPlayer.play();
+
+        hide();
+
+        if (mOnLayoutChangeListener == null) {
+            mOnLayoutChangeListener = new View.OnLayoutChangeListener() {
+                private final Runnable mRunnable = new Runnable() {
+                    @Override
+                    public void run() {
+                        updateVideoSurfaces();
+                    }
+                };
+                @Override
+                public void onLayoutChange(View v, int left, int top, int right,
+                                           int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+                    if (left != oldLeft || top != oldTop || right != oldRight || bottom != oldBottom) {
+                        mHandler.removeCallbacks(mRunnable);
+                        mHandler.post(mRunnable);
+                    }
+                }
+            };
+        }
+        mVideoSurfaceFrame.addOnLayoutChangeListener(mOnLayoutChangeListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        if (mOnLayoutChangeListener != null) {
+            mVideoSurfaceFrame.removeOnLayoutChangeListener(mOnLayoutChangeListener);
+            mOnLayoutChangeListener = null;
+        }
+
+        mMediaPlayer.stop();
+
+        mMediaPlayer.getVLCVout().detachViews();
+        mMediaPlayer.getVLCVout().removeCallback(this);
+    }
+    private void updateVideoSurfaces() {
+        if (mVideoWidth * mVideoHeight == 0)
+            return;
+        int sw = getWindow().getDecorView().getWidth();
+        int sh = getWindow().getDecorView().getHeight();
+
+        mMediaPlayer.getVLCVout().setWindowSize(sw, sh);
+        double dw = sw, dh = sh;
+        final boolean isPortrait = getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT;
+
+        if (sw > sh && isPortrait || sw < sh && !isPortrait) {
+            dw = sh;
+            dh = sw;
+        }
+
+        // sanity check
+        if (dw * dh == 0) {
+            Log.e(TAG, "Invalid surface size");
+            return;
+        }
+
+        // compute the aspect ratio
+        double ar, vw;
+        if (mVideoSarDen == mVideoSarNum) {
+            /* No indication about the density, assuming 1:1 */
+            vw = mVideoVisibleWidth;
+            ar = (double)mVideoVisibleWidth / (double)mVideoVisibleHeight;
+        } else {
+            /* Use the specified aspect ratio */
+            vw = mVideoVisibleWidth * (double)mVideoSarNum / mVideoSarDen;
+            ar = vw / mVideoVisibleHeight;
+        }
+
+        // compute the display aspect ratio
+        double dar = dw / dh;
+
+        switch (CURRENT_SIZE) {
+            case SURFACE_BEST_FIT:
+                if (dar < ar)
+                    dh = dw / ar;
+                else
+                    dw = dh * ar;
+                break;
+            case SURFACE_FIT_HORIZONTAL:
+                dh = dw / ar;
+                break;
+            case SURFACE_FIT_VERTICAL:
+                dw = dh * ar;
+                break;
+            case SURFACE_FILL:
+                break;
+            case SURFACE_16_9:
+                ar = 16.0 / 9.0;
+                if (dar < ar)
+                    dh = dw / ar;
+                else
+                    dw = dh * ar;
+                break;
+            case SURFACE_4_3:
+                ar = 4.0 / 3.0;
+                if (dar < ar)
+                    dh = dw / ar;
+                else
+                    dw = dh * ar;
+                break;
+            case SURFACE_ORIGINAL:
+                dh = mVideoVisibleHeight;
+                dw = vw;
+                break;
+        }
+
+        // set display size
+        ViewGroup.LayoutParams lp = mVideoSurface.getLayoutParams();
+        lp.width  = (int) Math.ceil(dw * mVideoWidth / mVideoVisibleWidth);
+        lp.height = (int) Math.ceil(dh * mVideoHeight / mVideoVisibleHeight);
+        mVideoSurface.setLayoutParams(lp);
+        if (mSubtitlesSurface != null)
+            mSubtitlesSurface.setLayoutParams(lp);
+
+        // set frame size (crop if necessary)
+        lp = mVideoSurfaceFrame.getLayoutParams();
+        lp.width = (int) Math.floor(dw);
+        lp.height = (int) Math.floor(dh);
+        mVideoSurfaceFrame.setLayoutParams(lp);
+
+        mVideoSurface.invalidate();
+        if (mSubtitlesSurface != null)
+            mSubtitlesSurface.invalidate();
+    }
+
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
+    @Override
+    public void onNewLayout(IVLCVout vlcVout, int width, int height, int visibleWidth, int visibleHeight, int sarNum, int sarDen) {
+        mVideoWidth = width;
+        mVideoHeight = height;
+        mVideoVisibleWidth = visibleWidth;
+        mVideoVisibleHeight = visibleHeight;
+        mVideoSarNum = sarNum;
+        mVideoSarDen = sarDen;
+        updateVideoSurfaces();
+    }
+
+    @Override
+    public void onSurfacesCreated(IVLCVout vlcVout) {
+    }
+
+    @Override
+    public void onSurfacesDestroyed(IVLCVout vlcVout) {
+    }
+
+    @Override
+    public void onHardwareAccelerationError(IVLCVout vlcVout) {
+    }
+}
