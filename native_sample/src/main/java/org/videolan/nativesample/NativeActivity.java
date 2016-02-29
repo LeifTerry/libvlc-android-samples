4462b0f408d9faa3b9df2c918594c4c80d17ddc7
Thomas Guillem <thomas@gllm.fr>
2016-02-29 10:02:25 +0100

    native_sample: simplify fullscreen

--- native_sample/src/main/java/org/videolan/nativesample/NativeActivity.java
+++ native_sample/src/main/java/org/videolan/nativesample/NativeActivity.java
@@ -59,19 +59,6 @@ public class NativeActivity extends AppCompatActivity implements IVLCVout.Callba
     private int mVideoSarNum = 0;
     private int mVideoSarDen = 0;
 
-    @TargetApi(19)
-    private void hide() {
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        mUiSurface.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
-
     private static int sInit = -1;
     static synchronized boolean loadLibraries(Context context) {
         if (sInit != -1)
@@ -151,8 +138,6 @@ public class NativeActivity extends AppCompatActivity implements IVLCVout.Callba
         if (mSubtitlesSurface != null)
             mAWindow.setSubtitlesView(mSubtitlesSurface);
         mAWindow.attachViews();
-
-        hide();
 
         if (mOnLayoutChangeListener == null) {
             mOnLayoutChangeListener = new View.OnLayoutChangeListener() {
