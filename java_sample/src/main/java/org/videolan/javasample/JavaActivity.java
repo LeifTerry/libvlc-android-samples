851899351216a0d3bc24a4261da6eecb8eca62d9
Thomas Guillem <thomas@gllm.fr>
2016-02-29 10:02:39 +0100

    java_sample: simplify fullscreen

--- java_sample/src/main/java/org/videolan/javasample/JavaActivity.java
+++ java_sample/src/main/java/org/videolan/javasample/JavaActivity.java
@@ -62,19 +62,6 @@ public class JavaActivity extends AppCompatActivity implements IVLCVout.Callback
     private int mVideoSarNum = 0;
     private int mVideoSarDen = 0;
 
-    @TargetApi(19)
-    private void hide() {
-        // Note that some of these constants are new as of API 16 (Jelly Bean)
-        // and API 19 (KitKat). It is safe to use them, as they are inlined
-        // at compile-time and do nothing on earlier devices.
-        mUiSurface.setSystemUiVisibility(View.SYSTEM_UI_FLAG_LOW_PROFILE
-                | View.SYSTEM_UI_FLAG_FULLSCREEN
-                | View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY
-                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
-                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION);
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -117,8 +104,6 @@ public class JavaActivity extends AppCompatActivity implements IVLCVout.Callback
         mMediaPlayer.setMedia(media);
         media.release();
         mMediaPlayer.play();
-
-        hide();
 
         if (mOnLayoutChangeListener == null) {
             mOnLayoutChangeListener = new View.OnLayoutChangeListener() {
