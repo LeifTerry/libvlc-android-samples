b68c595554d71468765fdfb98c07f30a2b966649
Thomas Guillem <thomas@gllm.fr>
2016-03-21 14:11:07 +0100

    native_sample: fix Android.mk

--- native_sample/jni/Android.mk
+++ native_sample/jni/Android.mk
@@ -5,11 +5,8 @@ LOCAL_MODULE    := native
 
 LOCAL_SRC_FILES := main.c
 
-LOCAL_LDFLAGS := -L${LIBVLC_LIBS}
-LOCAL_LDLIBS := -llog -lvlc
+LOCAL_LDLIBS := -L${LIBVLC_LIBS} -llog -lvlc
 
 LOCAL_C_INCLUDES := $(VLC_SRC_DIR)/include
-
-LOCAL_SHARED_LIBRARIES:= libvlc
 
 include $(BUILD_SHARED_LIBRARY)
